a9acc1f5b7f5fe704351c4d02d889bde41e1b453
Alister <2560533047@qq.com>
2020-05-09 12:42:19 +0800

    修改数据库字段

--- sql/schooltrade.sql
+++ sql/schooltrade.sql
@@ -1,260 +1,149 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- 主机： localhost
--- 生成日期： 2020-04-27 15:27:39
--- 服务器版本： 5.7.26
--- PHP 版本： 7.3.4
+/*
+Navicat MySQL Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+Source Server         : localhost_3306
+Source Server Version : 50553
+Source Host           : localhost:3306
+Source Database       : schooltrade
 
+Target Server Type    : MYSQL
+Target Server Version : 50553
+File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+Date: 2020-04-29 21:03:05
+*/
 
---
--- 数据库： `schooltrade`
---
+SET FOREIGN_KEY_CHECKS=0;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `goods`
---
-
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
 CREATE TABLE `goods` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL COMMENT '名称',
+  `pic_id` int(11) NOT NULL,
   `price` decimal(11,0) NOT NULL COMMENT '价格',
   `state` tinyint(1) NOT NULL COMMENT '是否上架',
   `info` varchar(300) COLLATE utf8_unicode_ci NOT NULL COMMENT '商品描述',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   `category_id` int(11) NOT NULL COMMENT '商品分类',
   `conditions` int(11) NOT NULL COMMENT '新旧程度',
-  `userid` int(11) NOT NULL COMMENT '用户'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `userid` int(11) NOT NULL COMMENT '用户',
+  `wechat` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- 转存表中的数据 `goods`
---
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+INSERT INTO `goods` VALUES ('20', '标题', '0', '250', '0', '<p>富文本编辑器</p>', '2020-04-29 21:01:43', '6', '5', '0', '微信');
 
-INSERT INTO `goods` (`id`, `name`, `price`, `state`, `info`, `create_time`, `category_id`, `conditions`, `userid`) VALUES
-(1, '卡西欧 G-SHOCK电子表G5600A', '450', 0, '买了新表这个表在家吃灰中', '2020-04-27 00:06:32', 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- 表的结构 `goods_category`
---
-
+-- ----------------------------
+-- Table structure for goods_category
+-- ----------------------------
+DROP TABLE IF EXISTS `goods_category`;
 CREATE TABLE `goods_category` (
-  `Id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '分类名'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '分类名',
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- 转存表中的数据 `goods_category`
---
+-- ----------------------------
+-- Records of goods_category
+-- ----------------------------
+INSERT INTO `goods_category` VALUES ('1', '图书');
+INSERT INTO `goods_category` VALUES ('2', '电脑');
+INSERT INTO `goods_category` VALUES ('3', '手机');
+INSERT INTO `goods_category` VALUES ('4', '电器');
+INSERT INTO `goods_category` VALUES ('5', '衣服');
+INSERT INTO `goods_category` VALUES ('6', '其他');
 
-INSERT INTO `goods_category` (`Id`, `name`) VALUES
-(1, '图书'),
-(2, '电脑'),
-(3, '手机'),
-(4, '电器'),
-(5, '衣服'),
-(6, '其他');
-
--- --------------------------------------------------------
-
---
--- 表的结构 `goods_conditions`
---
-
+-- ----------------------------
+-- Table structure for goods_conditions
+-- ----------------------------
+DROP TABLE IF EXISTS `goods_conditions`;
 CREATE TABLE `goods_conditions` (
-  `Id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '新旧程度'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '新旧程度',
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- 转存表中的数据 `goods_conditions`
---
+-- ----------------------------
+-- Records of goods_conditions
+-- ----------------------------
+INSERT INTO `goods_conditions` VALUES ('5', '全新');
+INSERT INTO `goods_conditions` VALUES ('6', '二手');
 
-INSERT INTO `goods_conditions` (`Id`, `name`) VALUES
-(5, '全新'),
-(6, '二手');
-
--- --------------------------------------------------------
-
---
--- 表的结构 `goods_pic`
---
-
+-- ----------------------------
+-- Table structure for goods_pic
+-- ----------------------------
+DROP TABLE IF EXISTS `goods_pic`;
 CREATE TABLE `goods_pic` (
-  `Id` int(11) NOT NULL COMMENT '图片表',
-  `goods_id` int(11) DEFAULT NULL,
+  `goods_id` int(11) NOT NULL AUTO_INCREMENT,
   `pic1` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `pic2` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `pic3` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `count` int(11) DEFAULT '0' COMMENT '图片数量'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `count` int(11) DEFAULT '0' COMMENT '图片数量',
+  PRIMARY KEY (`goods_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of goods_pic
+-- ----------------------------
+INSERT INTO `goods_pic` VALUES ('9', 'uploads/2020/04/29/5ea97aa687efe.jpg', null, null, '1');
 
---
--- 表的结构 `goods_state`
---
-
+-- ----------------------------
+-- Table structure for goods_state
+-- ----------------------------
+DROP TABLE IF EXISTS `goods_state`;
 CREATE TABLE `goods_state` (
-  `Id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '是否上架'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '是否上架',
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- 转存表中的数据 `goods_state`
---
+-- ----------------------------
+-- Records of goods_state
+-- ----------------------------
+INSERT INTO `goods_state` VALUES ('1', '已上架');
+INSERT INTO `goods_state` VALUES ('2', '未上架');
 
-INSERT INTO `goods_state` (`Id`, `name`) VALUES
-(1, '已上架'),
-(2, '未上架');
-
--- --------------------------------------------------------
-
---
--- 表的结构 `goods_tag`
---
-
+-- ----------------------------
+-- Table structure for goods_tag
+-- ----------------------------
+DROP TABLE IF EXISTS `goods_tag`;
 CREATE TABLE `goods_tag` (
-  `Id` int(11) NOT NULL COMMENT '标签',
+  `Id` int(11) NOT NULL AUTO_INCREMENT COMMENT '标签',
   `tag1` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `tag2` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `tag3` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+  `tag3` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of goods_tag
+-- ----------------------------
 
---
--- 表的结构 `user`
---
-
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL DEFAULT '',
   `password` varchar(32) NOT NULL DEFAULT '',
   `flag` tinyint(4) DEFAULT NULL COMMENT '是否管理员',
   `phone` int(11) NOT NULL,
   `regtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '注册时间',
   `avatar` varchar(30) NOT NULL COMMENT '头像',
-  `sex` int(11) NOT NULL COMMENT '性别'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `sex` int(11) NOT NULL COMMENT '性别',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `user`
---
-
-INSERT INTO `user` (`id`, `username`, `password`, `flag`, `phone`, `regtime`, `avatar`, `sex`) VALUES
-(2, 'admin', 'admin', 1, 0, '2020-04-24 13:44:07', '', 0),
-(3, 'wjh', 'wjh', 0, 0, '2020-04-24 13:44:07', '', 0),
-(4, 'zs', 'zs', 0, 0, '2020-04-24 13:44:07', '', 0);
-
---
--- 转储表的索引
---
-
---
--- 表的索引 `goods`
---
-ALTER TABLE `goods`
-  ADD PRIMARY KEY (`id`);
-
---
--- 表的索引 `goods_category`
---
-ALTER TABLE `goods_category`
-  ADD PRIMARY KEY (`Id`);
-
---
--- 表的索引 `goods_conditions`
---
-ALTER TABLE `goods_conditions`
-  ADD PRIMARY KEY (`Id`);
-
---
--- 表的索引 `goods_pic`
---
-ALTER TABLE `goods_pic`
-  ADD PRIMARY KEY (`Id`);
-
---
--- 表的索引 `goods_state`
---
-ALTER TABLE `goods_state`
-  ADD PRIMARY KEY (`Id`);
-
---
--- 表的索引 `goods_tag`
---
-ALTER TABLE `goods_tag`
-  ADD PRIMARY KEY (`Id`);
-
---
--- 表的索引 `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- 在导出的表使用AUTO_INCREMENT
---
-
---
--- 使用表AUTO_INCREMENT `goods`
---
-ALTER TABLE `goods`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- 使用表AUTO_INCREMENT `goods_category`
---
-ALTER TABLE `goods_category`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- 使用表AUTO_INCREMENT `goods_conditions`
---
-ALTER TABLE `goods_conditions`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- 使用表AUTO_INCREMENT `goods_pic`
---
-ALTER TABLE `goods_pic`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT COMMENT '图片表';
-
---
--- 使用表AUTO_INCREMENT `goods_state`
---
-ALTER TABLE `goods_state`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- 使用表AUTO_INCREMENT `goods_tag`
---
-ALTER TABLE `goods_tag`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT COMMENT '标签';
-
---
--- 使用表AUTO_INCREMENT `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('2', 'admin', 'admin', '1', '0', '2020-04-24 21:44:07', '', '0');
+INSERT INTO `user` VALUES ('3', 'wjh', 'wjh', '0', '0', '2020-04-24 21:44:07', '', '0');
+INSERT INTO `user` VALUES ('4', 'zs', 'zs', '0', '0', '2020-04-24 21:44:07', '', '0');
